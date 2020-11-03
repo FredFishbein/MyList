eea00e5060986f5f5cced3bcede4a07f585d3aba
2600 <a2600@Mac-AF.hsd1.ca.comcast.net>
2020-11-03 09:06:00 -0800

    111

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/", function(req,res){
+
+var today = new Date();
+
+var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+};
+
+
+var day = today.toLocaleDateString("en-US", options);
+// Up to this point we create the date and time
+
+
+    res.render("list", {KindOfDay: day, newListItems: items});
+
+});
+
+    app.post("/", function(req, res){
+         item = req.body.newItem;
+
+        items.push(item);
+
+        res.redirect("/");
+    });
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, function(){
+console.log("Port is listening on 3000")
+});
