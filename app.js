afcad9d7e3f4edbbc9bd7aec95eb02f7ae8959d6
2600 <a2600@Mac-AF.hsd1.ca.comcast.net>
2020-11-10 13:06:52 -0800

    Added

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+
+
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
-
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems =[];
 
 app.set('view engine', 'ejs');
 
@@ -12,35 +16,35 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/", function(req,res){
-
-var today = new Date();
-
-var options = {
-    weekday: "short",
-    day: "numeric",
-    month: "long",
-};
+let day = date();
 
 
-var day = today.toLocaleDateString("en-US", options);
-// Up to this point we create the date and time
-
-
-    res.render("list", {KindOfDay: day, newListItems: items});
+res.render("list", {listTitle: day, newListItems: items});
 
 });
 
     app.post("/", function(req, res){
-         item = req.body.newItem;
+        console.log(req.body);
+        let item = req.body.newItem;
 
-        items.push(item);
+        if (req.body.list === "Work"){
+            workItems.push(item);
+            res.redirect("/work");
+        }else{
+            items.push(item);
+            res.redirect("/");
+        }
 
-        res.redirect("/");
     });
 
 
+    app.get("/work", function(req,res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
 
-
+    });
+   app.get("/about", function(req,res){
+    res.render("about");
+   });
 
 
 
